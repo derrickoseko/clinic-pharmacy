7fbacc364176ab67c5f513e2bd99f1461c65cbdd
derrickoseko <33955073+derrickoseko@users.noreply.github.com>
2018-02-26 17:36:25 +0300

    pharmacy part of clinic system  review

--- src/doc/pharmacy.java
+++ src/doc/pharmacy.java
@@ -10,63 +10,98 @@ public class pharmacy extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton7 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jTextField7 = new javax.swing.JTextField();
+        jTextField6 = new javax.swing.JTextField();
+        jTextField5 = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setLayout(null);
 
-        jButton7.setText("supplier id");
-        jPanel1.add(jButton7);
-        jButton7.setBounds(240, 200, 110, 23);
-
-        jButton6.setText("item description");
-        jPanel1.add(jButton6);
-        jButton6.setBounds(240, 160, 110, 23);
-
-        jButton5.setText("item price");
-        jPanel1.add(jButton5);
-        jButton5.setBounds(240, 120, 110, 23);
-
-        jButton4.setText("item qty");
-        jPanel1.add(jButton4);
-        jButton4.setBounds(240, 80, 110, 23);
-
-        jButton3.setText("item unit");
-        jButton3.setActionCommand("item unit");
-        jPanel1.add(jButton3);
-        jButton3.setBounds(30, 160, 130, 23);
-
-        jButton2.setText("item id");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jTextField7.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jTextField7ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton2);
-        jButton2.setBounds(30, 120, 130, 23);
+        jPanel1.add(jTextField7);
+        jTextField7.setBounds(260, 160, 110, 20);
+        jPanel1.add(jTextField6);
+        jTextField6.setBounds(260, 120, 110, 20);
+        jPanel1.add(jTextField5);
+        jTextField5.setBounds(260, 80, 110, 20);
+
+        jLabel9.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
+        jLabel9.setText("item unit");
+        jPanel1.add(jLabel9);
+        jLabel9.setBounds(140, 170, 110, 20);
+
+        jLabel8.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
+        jLabel8.setText("item id");
+        jPanel1.add(jLabel8);
+        jLabel8.setBounds(140, 120, 110, 20);
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
+        jLabel7.setText("item name");
+        jPanel1.add(jLabel7);
+        jLabel7.setBounds(140, 80, 110, 20);
+        jPanel1.add(jTextField4);
+        jTextField4.setBounds(540, 210, 100, 20);
+        jPanel1.add(jTextField3);
+        jTextField3.setBounds(540, 170, 100, 20);
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
+        jLabel6.setText("supplier id");
+        jPanel1.add(jLabel6);
+        jLabel6.setBounds(428, 210, 80, 20);
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
+        jLabel5.setText("item description");
+        jPanel1.add(jLabel5);
+        jLabel5.setBounds(430, 170, 90, 20);
+        jPanel1.add(jTextField1);
+        jTextField1.setBounds(540, 90, 100, 20);
+
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jTextField2);
+        jTextField2.setBounds(540, 130, 100, 20);
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
+        jLabel4.setText("item price");
+        jPanel1.add(jLabel4);
+        jLabel4.setBounds(430, 130, 90, 20);
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
+        jLabel3.setText("item qty");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(430, 90, 90, 30);
 
         jLabel2.setFont(new java.awt.Font("MV Boli", 0, 36)); // NOI18N
         jLabel2.setText("Pharmacy");
         jPanel1.add(jLabel2);
         jLabel2.setBounds(30, 10, 290, 50);
 
-        jButton1.setText("item name");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jButton1);
-        jButton1.setBounds(30, 80, 130, 23);
+        jButton8.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
+        jButton8.setText("save");
+        jPanel1.add(jButton8);
+        jButton8.setBounds(340, 250, 80, 50);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/doc/a51e5abd1ca8b11fd0f7a4124174e0e4.jpg"))); // NOI18N
         jPanel1.add(jLabel1);
@@ -88,13 +123,13 @@ public class pharmacy extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jTextField7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField7ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField7ActionPerformed
 
     
     public static void main(String args[]) {
@@ -128,15 +163,23 @@ public class pharmacy extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField jTextField7;
     // End of variables declaration//GEN-END:variables
 }
